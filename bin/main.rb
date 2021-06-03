3f60f02ba9995769fd775b39610e64a69ea3d09f
joshuaivie <joshua@lifeonline.com.ng>
2021-06-03 13:52:43 -0100

    add valid argument check ARGV

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,17 @@
 #!/usr/bin/env ruby
+require 'colorize'
+require_relative '../lib/modules/linter_text'
 
 begin
+  file_path, *other_arguments = ARGV
+
+  if file_path.nil?
+    puts LinterText::NO_PATH
+    sleep(2)
+    exit
+  end
+
+  def fetch_file; end
 rescue Interrupt
   system('stty -echoctl')
   puts LinterText::INTERRUPT_MESSAGE
--- lib/modules/linter_text.rb
+++ lib/modules/linter_text.rb
@@ -1,3 +1,4 @@
 module LinterText
+  NO_PATH = ['No path provided'].freeze
   INTERRUPT_MESSAGE = ["\nExiting Gracefully..."].freeze
 end
