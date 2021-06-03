8dd7b9e10cdd95198f219cc6581fb9cae0d60c77
joshuaivie <joshua@lifeonline.com.ng>
2021-06-03 15:24:09 -0100

    add successful read

--- bin/main.rb
+++ bin/main.rb
@@ -1,17 +1,32 @@
 #!/usr/bin/env ruby
 require 'colorize'
 require_relative '../lib/modules/linter_text'
+require_relative '../lib/file_reader'
 
 begin
   file_path, *other_arguments = ARGV
 
   if file_path.nil?
-    puts LinterText::NO_PATH
+    puts LinterText::NO_PATH.colorize(:red)
     sleep(2)
     exit
   end
 
-  def fetch_file; end
+  def fetch_file(file_path)
+    file = FileReader.new(file_path)
+    if file.error
+      puts LinterText::BAD_PATH.colorize(:red)
+      exit
+    else
+      puts LinterText::SUCCESFUL_READ.colorize(:green)
+    end
+  end
+
+  at_exit do
+    puts LinterText::EXIT_MESSAGE.colorize(:green)
+  end
+
+  fetch_file(file_path)
 rescue Interrupt
   system('stty -echoctl')
   puts LinterText::INTERRUPT_MESSAGE
--- lib/modules/linter_text.rb
+++ lib/modules/linter_text.rb
@@ -1,4 +1,7 @@
 module LinterText
-  NO_PATH = ['No path provided'].freeze
-  INTERRUPT_MESSAGE = ["\nExiting Gracefully..."].freeze
+  NO_PATH = 'No path provided'.freeze
+  BAD_PATH = "The path provided is incorrect. Please correct it and try again.\n.\n".freeze
+  SUCCESFUL_READ = "File was succesfully read.\nAnalyzing file.\n...\n\n".freeze
+  EXIT_MESSAGE = "Exiting gracefully.\n".freeze
+  INTERRUPT_MESSAGE = "The process was interrupted ...\n".freeze
 end
