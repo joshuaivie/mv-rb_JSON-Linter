241b5264fb3b2c78d0f6b78d4a80df3b1020a6b1
joshuaivie <joshua@lifeonline.com.ng>
2021-06-09 18:02:03 -0100

    add #end_with_new_line_test
    Signed-off-by: joshuaivie <joshua@lifeonline.com.ng>

--- lib/file_tester.rb
+++ lib/file_tester.rb
@@ -1,23 +1,43 @@
+require_relative './modules/rules_json'
+
 class FileTester
   attr_reader :error_count
 
   def initialize(file)
-    @file_path = file.file_path
+    @file_name = file.file_name
+    @file_size = file.file_size
+    @file_content = file.file_content
     @error_location = []
     @error_classifiaction = []
-    @possible_resolution = []
-    @error_count = 0
+    @error_details = []
     @error_array = []
+    @error_count = 0
   end
 
   def run_tests
     test_results = {}
+
+    # run individual tests
+    end_with_new_line_test
+
+    # gather all errors
+
     test_results['errors'] = @error_array
     test_results['summary'] = test_summary
     test_results
   end
 
   private
+
+  def end_with_new_line_test
+    last_line = @file_content[-1]
+    return unless last_line.chars[-1] != "\n"
+
+    @error_count += 1
+    @error_location << "#{file_name}:#{file_size}:#{last_line.chars.length}:"
+    @error_classifiaction << JSONRules::NEWLINEERROR[0]
+    @error_details << JSONRules::NEWLINEERROR[1]
+  end
 
   def test_summary
     file_summary = '1 file'
---
+++ lib/modules/rules_json.rb
@@ -0,0 +1,3 @@
+module JSONRules
+  NEWLINEERROR = ['Stylistic/Newline', 'Expected to see a new line at the end of file'].freeze
+end
