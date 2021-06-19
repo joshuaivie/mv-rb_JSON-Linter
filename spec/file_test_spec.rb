1ffc4226c962fdfa61ee68adab217712d1b11e5d
joshuaivie <joshua@lifeonline.com.ng>
2021-06-19 15:51:13 +0100

    add Initialize context RSpec and upate bad_code.json

--- spec/file_test_spec.rb
+++ spec/file_test_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe FileTester do
   let(:file) { FileReader.new('./example/bad_code.json') }
   let(:tester) { FileTester.new(file) }
 
+  context 'Initialize' do
+    context 'when first initialized with a file object' do
+      it 'has an initial count of errors set to zero' do
+        expect(tester.error_count).to eql(0)
+      end
+    end
+  end
+
   context 'After file is tested' do
     describe '#run_tests' do
       it 'returns a hash with an errors key with all the errors raised' do
@@ -19,6 +27,9 @@ RSpec.describe FileTester do
       end
       it 'returns the errors summary as a string' do
         expect(tester.run_tests['summary'].class).to be String
+      end
+      it 'updates the error_count accessor' do
+        expect(tester.run_tests['errors'].size).to eql(tester.error_count)
       end
     end
   end
